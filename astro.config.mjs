3c9f690a54613986bc6f80aac4ef866f5c7ac94e
Mourad-ouerghi <mouraouerghi12@gmail.com>
2023-10-04 08:45:25 +0100

    fixed abug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import alpinejs from '@astrojs/alpine';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
 })
