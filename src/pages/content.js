14b24bef281cabb88a5fa11612d24ef830799d81
Mourad-ouerghi <mouraouerghi12@gmail.com>
2023-10-04 07:54:58 +0100

    fixed some design issues and some bugs

---
+++ src/pages/content.js
@@ -0,0 +1,178 @@
+// Personal Information --> #hero section
+export const name = "Mourad Ouerghi"
+export const designation = "Software Engineering Student"
+export const title= "AWS Solutions Architect - Associate Level"
+export const location = "Tunis, Tunisia"
+export const pronouns = ""
+export const linkendInLink = "https://www.linkedin.com/in/mourad-ouerghi-5b67aa224/"
+export const LinkedIn = "My linkedIn"
+export const buttonContent = "My Resume"
+
+// About
+export const about = "Hello, I'm Mourad Ouerghi a software engineering student 👨‍💻 at Faculty of Sciences of Tunisia, ambitious and hardworking person who’s always ready for new experiences, I've been a tech 💻 enthusiaste since highschool that's why I chose this career path. Currently my interests are shifted towards web development, cloud ☁ and DevOps 🐳. Feel free to contact me if you have any questions or if you want to work with me 😃. (Oh have I mentionned that I love anime? my favourite anime is HunterXHunter🤫)"
+
+// Work Experience --> #work section
+export const work = [
+	{
+		from: "July 2023",
+		to: "Present",
+		role: "Backend Developer Intern",
+		org: "Hi Interns",
+		org_url: "https://hi-interns.com/",
+		location: "Tunis, Tunisia",
+		skills: "Event driven architecture, Webhooks, Cron Jobs, Automations, Strapi, Airatble, Supabase, Netlify"
+	},
+	{
+		from: "October 2022",
+		to: "May 2023",
+		role: "Product Owner",
+		org: "Optima Junior Entreprise",
+		org_url: "https://optimaje.com/",
+		location: "Tunis, Tunisia",
+		skills: "Requirements Gathering, Client Negotiations"
+	},
+	{
+		from: "June 2022",
+		to: "July 2022",
+		role: "Summer Internship",
+		org: "National Center of Computing",
+		org_url: "http://www.cni.tn/index.php/fr/",
+		location: "Tunis, Tunisia",
+		skills: " Introduction to classic hosting services such as VPS provisioning and modern ones such as using Docker forcontainerization and kubernetes for orchestration"
+	},
+	{
+		from: "February 2022",
+		to: "June 2022",
+		role: "Project Manager Assistant",
+		org: "Optima Junior Entreprise",
+		org_url: "https://optimaje.com/",
+		location: "Tunis, Tunisia",
+		skills: "Project Management (Agile), Team Management, Client Meetings"
+	},
+	{
+		from: "October 2021",
+		to: "Feb. 2022",
+		role: "IT Consultant",
+		org: "Optima Junior Entreprise",
+		org_url: "https://optimaje.com/",
+		location: "Tunis, Tunisia",
+		skills: "Web development, Deployments, Client Meetings, Requirments Gathering, Wordpress"
+	},
+]
+
+// Projects --> #project section
+export const projects = [
+	{
+		title: "Deploying a highly availabe application in AWS",
+		description: "Deployment of a springboot application in a highly available and scalable architecture using AWS services.",
+		url: "https://drive.google.com/file/d/12zfWN4FQZ8S3g3xWPMdIsl-38XyfH8eK/view?usp=sharing",
+		tech: "AWS Services(EC2, VPC, Autoscaling, Loadbalancer, LaunchConfigurations), Springboot, Git, GitHub",
+	},
+	{
+		title: "Web-based Event Management Application",
+		description: "A web application allowing an administrator to create, modify, and delete events, and a visitor to browse the various events.",
+		url: "https://github.com/Mourad-Ouerghi/event-management",
+		tech: "MongoDB Atlass, ExpressJS, ReactJS, Node.js, Git, GitHub",
+	},
+	{
+		title: "File organization tool",
+		description: "A tool that automates the organization of files based on their extension.",
+		url: "https://github.com/Mourad-Ouerghi/files-organizer",
+		tech: "Python, Bashscript, Git, GitHub",
+	},
+	{
+		title: "TicTacToe Against AI",
+		description: "Tackle some of the fundamental algorithms of artificial intelligence: \"Min-Max\" and \"Alpha-Beta\" algorithms through the Tic-Tac-Toe game, to understand the difference between these two algorithms in terms of resource consumption",
+		url: "https://github.com/Mourad-Ouerghi/AI-TicTacToe",
+		tech: "Javascript, Html, Css",
+	},
+]
+
+// Education --> #education section
+export const education = [
+	{
+		from: "2021",
+		to: "Present",
+		institute: "Faculty Of Sciences Tunisia",
+		course: "Software Engineering",
+		location: "Tunis, Tunisia"
+	},
+	{
+		from: "2019",
+		to: "2021",
+		institute: "Faculty Of Sciences Tunisia",
+		course: "Preparatory Cycle for Engineering Studies",
+		location: "Tunis, Tunisia"
+	},
+	{
+		from: "2018",
+		to: "2019",
+		institute: "Sadiki High School",
+		course: "Mathemetical Bacalaureate",
+		location: "Tunis, Tunisia"
+	}
+]
+
+// Contact --> #contact section
+export const contact = [
+	{
+		source_name: "ouerghii.mourad@gmail.com",
+		source: "#",
+	},
+	{
+		source_name: "Github",
+		source: "https://github.com/Mourad-Ouerghi",
+	},
+	{
+		source_name: "LinkedIn",
+		source: "https://www.linkedin.com/in/mourad-ouerghi-5b67aa224",
+	},
+]
+
+// Certifications --> #certificate section
+
+export const certificates = [
+	{
+		issued: "2023",
+		title: "AWS Solutions Architect - Associate",
+		org: "Amazon Web Services",
+		url: "https://www.credly.com/badges/6e8576a2-1ef4-485e-80eb-399adfbf7a53/public_url",
+		description: "A comprehensive understanding of AWS services and technologies. The ability to build secure and robust solutions using architectural design principles based on customer requirements.",
+	},
+	{
+		issued: "2022",
+		title: "AWS Cloud Practitioner Essentials",
+		org: "Amazon Web Services",
+		url: "https://www.credly.com/badges/ed811192-35d2-409c-ba3a-b52cb8bfe92c/linked_in",
+		description: "Fundamental understanding of IT services and their uses in the AWS Cloud and foundational AWS knowledge.",
+	},
+]
+
+// Blogs --> #blogs section
+export const blogs = [
+
+]
+
+// Achievements --> #achievement section
+export const achievements = [
+	{
+		title: "Java tutor at Optima Junior Entreprise",
+		year: "2023",
+		decription: "Introduction to Java programming language and Object Oriented Programming for the new members of the association."
+	},
+	{
+		title: "Member of the Aiesec association",
+		year: "2022",
+		decription: " Management of the various exchange participants as well as attendance at international meetings with other Aiesec members to set exchange goals."
+	},
+	{
+		title: "Member of the Aiesec association",
+		year: "2022",
+		decription: " Management of the various exchange participants as well as attendance at international meetings with other Aiesec members to set exchange goals."
+	},
+	{
+		title: "Member of the organizing team of IP Days event held by REDSTART",
+		year: "2022",
+		decription: "Honored for promoting safety awareness and education in the Quahog community."
+	},
+]
